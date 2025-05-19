b44faab3d8b8160619bee55aff337c5a24fddf3c
NetherDevil <80260541+NetherDevil@users.noreply.github.com>
2025-05-19 13:51:43 +0800

    Update install_vcpkg_dependencies.sh

--- CI/install_vcpkg_dependencies.sh
+++ CI/install_vcpkg_dependencies.sh
@@ -2,6 +2,7 @@
 
 RELEASE_TAG="v1.8"
 FILENAME="dependencies-$1"
-DOWNLOAD_URL="https://github.com/vcmi/vcmi-deps-windows/releases/download/$RELEASE_TAG/$FILENAME.txz"
+DOWNLOAD_REPO="https://github.com/NetherDevil/vcmi-deps-windows-arm64"
+DOWNLOAD_URL="$DOWNLOAD_REPO/releases/download/$RELEASE_TAG/$FILENAME.txz"
 
 curl -L "$DOWNLOAD_URL" | tar -xf - --xz
